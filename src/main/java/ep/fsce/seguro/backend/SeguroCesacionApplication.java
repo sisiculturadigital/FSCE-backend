4257d487538d8aae95ca0fb779c73cf177380de9
JOSEPH RANDY VEGA DIAZ <jaguilar.analista.ca>
2022-11-21 21:58:15 -0500

    change jar to WAR

--- src/main/java/ep/fsce/seguro/backend/SeguroCesacionApplication.java
+++ src/main/java/ep/fsce/seguro/backend/SeguroCesacionApplication.java
@@ -2,10 +2,17 @@ package ep.fsce.seguro.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SeguroCesacionApplication {
+public class SeguroCesacionApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SeguroCesacionApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SeguroCesacionApplication.class, args);
 	}
